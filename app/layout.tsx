7de7b5e42f00e1866ce71a9dc6d2c21f43c65ae7
watackeech <devkeech@gmail.com>
2024-06-21 11:43:10 +0900

    add: GTM Event

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-N39P33X6" />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { sendGTMEvent } from "@next/third-parties/google";
 import Image from "next/image";
 
 export default function Home() {
@@ -107,6 +109,22 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <button
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          rel="noopener noreferrer"
+          onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Test{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            This is a test button for GTM
+          </p>
+        </button>
       </div>
     </main>
   );
